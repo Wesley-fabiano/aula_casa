97f549f2cbda5c5fec723f1106ab3666788ef4c2
wesley-laquila <wesley@laquila.com.br>
2025-04-16 08:27:23 -0300

    alterando app

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 nome = input('Digite seu nome: ')
 
-print(nome)
+idade = 36
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
+# Abordagem do .format()
+print('Meu nome é {} e tenho {} anos.'.format(nome,idade))
